0b82da7deb5c061f3e32a0923ea7dd5910845ef2
Unknown <cass465andres@gmail.com>
2018-02-15 12:30:53 -0500

    a>b

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main{
+	public static void main(String[] arg){
+		int a=20;
+		int b=10;
+		System.out.println("a="+a+" || b="+b);
+		System.out.println("a="+a+" es mayor que b="+b);
+	}
+}
